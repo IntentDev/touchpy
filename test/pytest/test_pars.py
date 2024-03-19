db953ca2ab9c7deca55bb3b6be9e4b081613aa6e
Idzard Kwadijk <idzard@intentdev.io>
2024-03-19 11:13:16 +0100

    passings tests for most pars

--- test/pytest/test_pars.py
+++ test/pytest/test_pars.py
@@ -1,67 +1,161 @@
 import pytest
-import pathlib
+from pathlib import Path
 import sys
+import keyboard
+import json
 
-localImportPath = pathlib.Path(__file__).parents[2] / 'out/build/x64-release'
+localImportPath = Path(__file__).parents[2] / 'out/build/x64-release'
 if str(localImportPath) not in sys.path:
 	sys.path.insert(0,str(localImportPath))	
 
 import touchpy as tp
 
 class TestClass:
-	def __init__(self):
-		self.running = True # used to gracefully exit the loop
-		self.frame = 0
-		self.runComp('../tox/test.tox')
-		
+	
+	@classmethod
+	def setup_class(cls):
+		"""Setup any state specific to the execution of the given class.
+		"""
+		print("setup class...")
+		test = TestClass()
+		cls.frame = 0
+		cls.done = False
+		cls.runComp(cls, '../tox/test.tox')
+		cls.referenceFile = Path(__file__).parents[1] / 'tox/parvalues.json'
+		cls.referenceValues = json.loads(cls.referenceFile.read_text())
+	
+	@classmethod
+	def teardown_class(cls):
+		""""Teardown any state that was previously setup with a setup_class method.
+		"""
+		print("stopping class")
+		cls.done = True
+
+
+	def test_float(self):
+		parname = "Float"
+		print(f"testing {parname}...")
+		val = self.comp.par[parname].val
+		ref = self.referenceValues["Test"][parname]["val"]
+		assert val == ref
+
+	def test_float2(self):
+		parname = "Float2"
+		print(f"testing {parname}...")
+		val = [self.comp.par[parname].x, self.comp.par[parname].y]
+		ref = self.referenceValues["Test"][parname]["val"]
+		assert val == ref
+
+	def test_float3(self):
+		parname = "Float3"
+		print(f"testing {parname}...")
+		val = [self.comp.par[parname].x, self.comp.par[parname].y, self.comp.par[parname].z]
+		ref = self.referenceValues["Test"][parname]["val"]
+		assert val == ref
+
+	def test_float4(self):
+		parname = "Float4"
+		print(f"testing {parname}...")
+		val = [self.comp.par[parname].x, self.comp.par[parname].y, self.comp.par[parname].z, self.comp.par[parname].w]
+		ref = self.referenceValues["Test"][parname]["val"]
+		assert val == ref
+
+	def test_int(self):
+		parname = "Int"
+		print(f"testing {parname}...")
+		val = self.comp.par[parname].val
+		ref = self.referenceValues["Test"][parname]["val"]
+		assert val == ref
+	
+	def test_int2(self):
+		parname = "Int2"
+		print(f"testing {parname}...")
+		val = [self.comp.par[parname].x, self.comp.par[parname].y]
+		ref = self.referenceValues["Test"][parname]["val"]
+		assert val == ref
+
+	def test_int3(self):
+		parname = "Int3"
+		print(f"testing {parname}...")
+		val = [self.comp.par[parname].x, self.comp.par[parname].y, self.comp.par[parname].z]
+		ref = self.referenceValues["Test"][parname]["val"]
+		assert val == ref
+
+	def test_int4(self):
+		parname = "Int4"
+		print(f"testing {parname}...")
+		val = [self.comp.par[parname].x, self.comp.par[parname].y, self.comp.par[parname].z, self.comp.par[parname].w]
+		ref = self.referenceValues["Test"][parname]["val"]
+		assert val == ref
+
+	def test_rgb(self):
+		parname = "Rgb"
+		print(f"testing {parname}...")
+		val = [self.comp.par[parname].r, self.comp.par[parname].g, self.comp.par[parname].b]
+		ref = self.referenceValues["Test"][parname]["val"]
+		assert val == ref
+
+	def test_rgb(self):
+		parname = "Rgba"
+		print(f"testing {parname}...")
+		val = [self.comp.par[parname].r, self.comp.par[parname].g, self.comp.par[parname].b, self.comp.par[parname].a]
+		ref = self.referenceValues["Test"][parname]["val"]
+		assert val == ref
+
+	def test_string(self):
+		parname = "Str"
+		print(f"testing {parname}...")
+		val = self.comp.par[parname].val
+		ref = self.referenceValues["Test"][parname]["val"]
+		assert val == ref
+
+
+	
 	@staticmethod
 	def on_frame(comp, this):
 
-		# print out all the parameters name on the first frame
-		parNames = comp.par.names()
-		if (this.frame == 0):
-			for name in parNames:
-				print(f"{name}: {comp.par[name].val}")
-			
+		# optional used to quit if comp.start() is called
+		if (keyboard.is_pressed('q')):
+			print("pressed q key")
+			comp.stop()
+			return
 		
-		# comp.start_next_frame() is only needed if comp.start(start_next_frame=False)
-		# or comp.update(call_start_next_frame=False), they are both False by default
-		#
-		# this allows the user to control when the next frame starts and so they can do work on non-comp members
-		# if comp.start(True) or comp.update(True) is called, start_next_frame() will be called automatically
-		# and shouldn't be called here
-		if comp.start_next_frame():
+		if this.done == True:
+			print("done is true")
+			comp.stop()
+			return
+		
+		#if comp.start_next_frame():
 			# do work on non-comp members for the next frame here
-			pass
-		else:
+		#	pass
+		#else:
 			# we have a problem with TouchEngine starting the next frame.
 			# Currently the error will be printed in the console but start_next_frame()
 			# will need to be updated to return an enum with the result for error handling
 			# in the future
-			pass
+			#pass
 
+		
 		this.frame += 1
+		
 
 	def runComp(self, tox_path):
+		print("starting tox...")
 		# create a comp object and specify a path to a tox file
-		comp = tp.Comp(tox_path)
+		self.comp = tp.Comp(tox_path)
 
 		# set the on_frame callback
 		# first are is static method (or free function) and second is the object to be passed to the callback
 		# in this case, self is passed to the callback so we can access the class members directly
-		comp.set_on_frame_callback(self.on_frame, self)
+		self.comp.set_on_frame_callback(self.on_frame, self)
 
 	
 		# this runs a loop that calls update() internally
-		#comp.start() # need to manually call start_next_frame() in on_frame callback to start next frame
-		comp.start(True) # update function starts next frame automatically
+		#self.comp.start() # need to manually call start_next_frame() in on_frame callback to start next frame
+		self.comp.start(True) # update function starts next frame automatically
 
-		# run the loop in Python anc call update manually
-		# while not (keyboard.is_pressed('q')):
-		#	# comp.update(True) # update function starts next frame
-		# 	comp.update() #  need to manually call start_next_frame() in on_frame callback to start next frame
-		pass
+		
 
 # create an instance of a class that runs the comp
-test = TestClass()
+#test = TestClass()
 
