4cc0d1a47f509223fd292e29e420e1ec52485bad
Idzard Kwadijk <idzard@intentdev.io>
2024-03-27 06:42:50 +0100

    basic working version

--- examples/yolov8/yolo-free.py
+++ examples/yolov8/yolo-free.py
@@ -1,5 +1,7 @@
-from ultralytics import YOLO
+#from ultralytics import YOLO
+import torch 
+print(torch.__version__)
+print(torch.cuda.is_available())
+#model = YOLO("yolov8m-pose.pt",verbose=True)
 
-model = YOLO("yolov8m-pose.pt",verbose=True)
-
-results = model(source=0, show=True, conf=0.3, save=True)
+#results = model(source=0, show=True, conf=0.3, save=True)
--- examples/yolov8/yolo.py
+++ examples/yolov8/yolo.py
@@ -5,17 +5,21 @@ from pathlib import Path
 import sys
 
 from ultralytics import YOLO
+
 localImportPath = Path(__file__).parents[2] / 'out/build/x64-release'
 if str(localImportPath) not in sys.path:
 	sys.path.insert(0,str(localImportPath))	
 
 import touchpy as tp
 
+torch.cuda.set_device(0)
+
 class ExampleRunComp:
 	def __init__(self):
 		self.running = True # used to gracefully exit the loop
 		self.frame = 0
-		self.model = YOLO("yolov8m-pose.pt")
+		self.model = YOLO("models/yolov8m-pose.pt")
+		
 
 
 	@staticmethod
@@ -27,15 +31,28 @@ class ExampleRunComp:
 			return
 		if (this.frame == 1):
 			print("tox started")
-		tensor = comp.out_tops[2].as_tensor()
-		if (this.frame == 2):
-				print("tensor shape: ", tensor.shape, "tensor dtype: ", tensor.dtype, 
-				"tensor device: ", tensor.device, "tensor layout: ", tensor.layout, 
-				"tensor strides: ", tensor.stride(), "tensor is_contiguous: ", tensor.is_contiguous())
+		tensor = comp.out_tops[0].as_tensor()
 		
-		results = this.model(tensor.unsqueeze(0), show=True, conf=0.3)
-		comp.in_tops[0].from_tensor(tensor)
-		print(this.frame)		
+		tensor.half()
+
+		# convert an rgba tensor to a rgb tensor
+		if (tensor.shape[0] == 4):
+			tensor = tensor[:3]	
+
+
+
+		# flip a tensor image upside down
+		tensor = torch.flip(tensor, [1])
+			
+		
+		
+		results = this.model(tensor.unsqueeze(0), show=True, stream=True, device=0)
+		for result in results:
+			print(result.keypoints)
+			#annotatedArray = r.plot()
+			#comp.in_tops[0].from_tensor(torch.from_numpy(annotatedArray))
+			#comp.in_tops[0].from_tensor(tensorOrg)
+				
 		this.frame += 1
 
 	def runComp(self, tox_path):
@@ -45,13 +62,13 @@ class ExampleRunComp:
 
 		comp.set_on_frame_callback(self.on_frame, self)
 
-		comp.start() # start the comp, blocks with RunMode.InternalTimeAuto and RunMode.InternalTimeSemiAuto
-		comp.unload() # should be called to properly unload the comp (especially if Python exits immediately after this)
+		comp.start()
+		comp.unload()
 		pass
 
 # create an instance of a class that runs the comp
 example = ExampleRunComp()
 
 # run the comp
-example.runComp('TopChopDatIO.tox')
+example.runComp('tox/yolo.tox')
 
