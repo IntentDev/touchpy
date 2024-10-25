f782281a158c82984c05b13743bbb50397ebf957
keithlostracco <keith@intentdev.io>
2024-10-25 09:22:11 -0700

    update callbacks in examples with no info parameter/arg

--- package/examples/basics/05_dats.py
+++ package/examples/basics/05_dats.py
@@ -5,8 +5,8 @@ import numpy as np
 class MyComp (tp.Comp):
 	def __init__(self):
 		super().__init__()
-		self.set_on_layout_change_callback(self.on_layout_change, {})
-		self.set_on_frame_callback(self.on_frame, {})
+		self.set_on_layout_change_callback(self.on_layout_change)
+		self.set_on_frame_callback(self.on_frame)
 
 		self.frame = 0
 
@@ -24,7 +24,7 @@ class MyComp (tp.Comp):
 
 
 
-	def on_layout_change(self, info):
+	def on_layout_change(self):
 		print('layout changed:')
 		print('out dats:\n', *[f"\t{name}\n" for name in self.out_dats.names])
 		print('in dats:\n', *[f"\t{name}\n" for name in self.in_dats.names])
@@ -35,7 +35,7 @@ class MyComp (tp.Comp):
 			# self.par['Openwindow'].pulse()
 			pass
 
-	def on_frame(self, info):
+	def on_frame(self):
 		if utils.check_key('q'):
 			self.stop()
 			return
