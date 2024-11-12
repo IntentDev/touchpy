1857bd1eb1a16c61298aff770bbe134ed92ed237
keithlostracco <keith@intentdev.io>
2024-11-11 19:06:10 -0700

    remove old nanobind

--- build.bat
+++ build.bat
@@ -23,30 +23,30 @@ ninja -C out/install_build/py312
 
 pushd %~dp0
 
-REM Command file for Sphinx documentation
+@REM REM Command file for Sphinx documentation
 
-if "%SPHINXBUILD%" == "" (
-	set SPHINXBUILD=sphinx-build
-)
-set SOURCEDIR=docs/source
-set BUILDDIR=install/docs
+@REM if "%SPHINXBUILD%" == "" (
+@REM 	set SPHINXBUILD=sphinx-build
+@REM )
+@REM set SOURCEDIR=docs/source
+@REM set BUILDDIR=install/docs
 
-%SPHINXBUILD% >NUL 2>NUL
-if errorlevel 9009 (
-	echo.
-	echo.The 'sphinx-build' command was not found. Make sure you have Sphinx
-	echo.installed, then set the SPHINXBUILD environment variable to point
-	echo.to the full path of the 'sphinx-build' executable. Alternatively you
-	echo.may add the Sphinx directory to PATH.
-	echo.
-	echo.If you don't have Sphinx installed, grab it from
-	echo.https://www.sphinx-doc.org/
-	exit /b 1
-)
+@REM %SPHINXBUILD% >NUL 2>NUL
+@REM if errorlevel 9009 (
+@REM 	echo.
+@REM 	echo.The 'sphinx-build' command was not found. Make sure you have Sphinx
+@REM 	echo.installed, then set the SPHINXBUILD environment variable to point
+@REM 	echo.to the full path of the 'sphinx-build' executable. Alternatively you
+@REM 	echo.may add the Sphinx directory to PATH.
+@REM 	echo.
+@REM 	echo.If you don't have Sphinx installed, grab it from
+@REM 	echo.https://www.sphinx-doc.org/
+@REM 	exit /b 1
+@REM )
 
-%SPHINXBUILD% -M html %SOURCEDIR% %BUILDDIR% %SPHINXOPTS% %O%
+@REM %SPHINXBUILD% -M html %SOURCEDIR% %BUILDDIR% %SPHINXOPTS% %O%
 
-:end
+@REM :end
 popd
 
 @REM if first argument exists use it as a path to copy modules, docs, and examples to the specified directory
